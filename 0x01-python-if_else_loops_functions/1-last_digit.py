e9634e14000d8437745655ef4f0c96a3671ed377
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-02 19:53:50 +0000

    changed tabs to spaces :(

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 0: 
-	lastNum = number % 10
+    lastNum = number % 10
 elif number <= 0:
-	lastNum = number % -10
+    lastNum = number % -10
 print('Last digit of {:d} is {:d}'. format(number, lastNum), end=" ")
 if lastNum > 5:
-	print('and is greater than 5')
+    print('and is greater than 5')
 elif lastNum == 0:
-	print('and is 0')
+    print('and is 0')
 elif lastNum < 6:
-	print('and is less than 6 and not 0')
+    print('and is less than 6 and not 0')
