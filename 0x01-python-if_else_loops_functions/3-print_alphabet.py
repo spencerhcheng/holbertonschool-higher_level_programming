01a51ff44863a578d19bbf134306ed8790fcb973
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-02 20:31:53 +0000

    remove wrong file name

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-letter = 97
-
-while (letter < 123):
-    if letter == 101 or letter == 113:
-        letter = letter + 1
-        continue
-    print('{}'.format(chr(letter)), end="")
-    letter = letter + 1
