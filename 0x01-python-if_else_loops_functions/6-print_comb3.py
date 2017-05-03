4892638d7a09bffa99875cfabb3db8c705df8f31
Spencer <spencerhcheng@gmail.com>
2017-05-03 02:15:19 +0000

    fixed whitespace and accounted for last printed number, 89

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-num1 = 1
-num2 = 1
 
-while num1 < 10 and num1 != num2:
-    print('{:d}'.format(num1), end="")
-    num1 = num1 + 1
-    while num2 < 10 and num2 != num1:
-        print('{:d}'.format(num2), end="")
-        num2 = num2 + 1
+for num1 in range(10):
+    for num2 in range(1 + num1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{:d}".format(89))
+        else:
+            print('{:d}{:d}'.format(num1, num2), end=", ")
