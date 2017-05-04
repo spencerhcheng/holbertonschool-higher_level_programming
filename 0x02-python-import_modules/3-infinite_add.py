3abf2157fb39a003f127a2fa7ef205ca257be7dd
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-04 21:49:05 +0000

    removed lines 5, 6: do not need to account for if no arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,6 @@ if __name__ == "__main__":
     import sys
     sum = 0
     argc = len(sys.argv)
-    if argc == 1:
-        print('0')
-    else:
-        for i in range(1, argc):
-            sum = sum + int(sys.argv[i])
-    print(sum)
+    for i in range(1, argc):
+        sum = sum + int(sys.argv[i])
+print(sum)
