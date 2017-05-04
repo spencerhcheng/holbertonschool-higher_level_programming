9f35f5ed60d216b935a3093f71587a81ceff9a83
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-04 20:40:57 +0000

    added print on line 13

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ cmdLength = len(sys.argv)
 if cmdLength == 1:
     print('{:d} argument.'.format(cmdLength - 1))
 elif cmdLength == 2:
-    print((cmdLength - 1), 'argument:', str(sys.argv[1]))
+    print((cmdLength - 1), 'argument:')
+    print(cmdLength - 1, ':', str(sys.argv[1]))
 elif cmdLength > 2:
     print('{:d} arguments:'.format(cmdLength - 1))
     for i in range(1, cmdLength):
