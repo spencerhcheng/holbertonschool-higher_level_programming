79912c81d4213e165f4815a82259adacd27e189f
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-04 21:36:27 +0000

    fixed to remove space before semicolons

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print('{:d} argument.'.format(cmdLength - 1))
     elif cmdLength == 2:
         print((cmdLength - 1), 'argument:')
-        print(cmdLength - 1, ':', str(sys.argv[1]))
+        print('{:d}: {}'. format(cmdLength - 1, str(sys.argv[1])))
     elif cmdLength > 2:
         print('{:d} arguments:'.format(cmdLength - 1))
         for i in range(1, cmdLength):
-            print(i, ":", str(sys.argv[i]))
+            print('{}: {}'.format(i, str(sys.argv[i])))
