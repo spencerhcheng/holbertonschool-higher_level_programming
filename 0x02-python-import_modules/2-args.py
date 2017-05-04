68f79d242556908f81d8af97044eee687e76b5b1
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-04 20:42:44 +0000

    add line 2: code should not be executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-cmdLength = len(sys.argv)
-if cmdLength == 1:
-    print('{:d} argument.'.format(cmdLength - 1))
-elif cmdLength == 2:
-    print((cmdLength - 1), 'argument:')
-    print(cmdLength - 1, ':', str(sys.argv[1]))
-elif cmdLength > 2:
-    print('{:d} arguments:'.format(cmdLength - 1))
-    for i in range(1, cmdLength):
-        print(i, ":", str(sys.argv[i]))
+    cmdLength = len(sys.argv)
+    if cmdLength == 1:
+        print('{:d} argument.'.format(cmdLength - 1))
+    elif cmdLength == 2:
+        print((cmdLength - 1), 'argument:')
+        print(cmdLength - 1, ':', str(sys.argv[1]))
+    elif cmdLength > 2:
+        print('{:d} arguments:'.format(cmdLength - 1))
+        for i in range(1, cmdLength):
+            print(i, ":", str(sys.argv[i]))
