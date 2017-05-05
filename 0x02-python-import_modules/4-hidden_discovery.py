7f7039236a3fdb25337bda770ea5f8143908a561
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-05 02:02:19 +0000

    sort and only print first 3 names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys, hidden_4
-
+    import sys, hidden_4
+    names = dir(hidden_4)
+    for dir in names:
+        if dir[:2] != "__":
+            print(dir)
