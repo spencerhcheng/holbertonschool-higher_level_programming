d8c4a2d4bcc5166d413c11b4965ad59e4e2d974e
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 23:53:04 +0000

    adding main files

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = None
+
+#    if (len(my_list)) <= 0:
+#        return None
+
+    for i in my_list:
+        if my_list > max:
+            max = mylist
+    return max
