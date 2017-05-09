84baa1fd19b46b48f20fe3ee77a71d04048ccd4d
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 20:53:50 +0000

    added condition for if idx is less than 0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 def element_at(my_list, idx):
     str_len = len(my_list)
-    if idx > str_len:
-        return (None)
 
-    return(my_list[idx])
+    if idx >= str_len or idx < 0:
+        return None
+
+    return (my_list[idx])
