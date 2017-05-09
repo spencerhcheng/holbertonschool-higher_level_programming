813b907c19d584c52dd18043bbda60c5661730f3
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 20:55:47 +0000

    removed declaration line for str_len

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 def element_at(my_list, idx):
-    str_len = len(my_list)
-
-    if idx >= str_len or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return None
 
     return (my_list[idx])
