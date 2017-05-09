6ab4a1c8116f240cc2661aac83baebd4e0dd07be
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 21:53:11 +0000

    4-new_in_list.py: initial commit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+def new_in_list(my_list, idx, element):
+
+    list_cpy = my_list[:]
+    if idx > len(my_list):
+        return my_list
+
+    else:
+        list_cpy[idx] = element
+        return list_cpy
