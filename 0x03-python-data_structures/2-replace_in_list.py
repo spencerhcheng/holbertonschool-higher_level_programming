9782513d23a934626c1def6369e0c2e1fbb879a7
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 22:03:05 +0000

    edit condition

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    list_len = len(my_list)
-    if idx > list_len:
+    if idx >= len(my_list):
         return my_list
 
     else:
