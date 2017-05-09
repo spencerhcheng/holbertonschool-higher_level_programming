d9b296cf360c493ce2ac8ccaa97825f6fa92698e
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-09 21:35:40 +0000

    2-replace_in_list.py: initial commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+def replace_in_list(my_list, idx, element):
+    list_len = len(my_list)
+    if idx > list_len:
+        return my_list
+
+    else:
+        my_list[idx] = element
+        return my_list
