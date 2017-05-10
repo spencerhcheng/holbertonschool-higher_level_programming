6ff3bcfb56a8f981dde8c005701b688177fa3eea
Spencer Cheng <spcheng0412@gmail.com>
2017-05-10 05:11:01 +0000

    append to new_list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,10 @@
 def divisible_by_2(my_list=[]):
 
     if my_list is not None:
-        new_list = ""
+        new_list = []
         for i, val in enumerate(my_list):
-            if val % 2 ==  0:
-                new_list[i] = 'True'
-            elif vale % 2 != 0:
-                new_list[i] = 'False'
+            if val % 2 == 0:
+                new_list.append(True)
+            elif val % 2 != 0:
+                new_list.append(False)
+    return new_list
