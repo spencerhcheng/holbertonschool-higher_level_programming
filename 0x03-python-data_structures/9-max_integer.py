b2a77d8cf0f8024e09e567a5541d1f767c022abf
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-10 01:52:46 +0000

    if list is empty, return None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+
     if my_list is not None:
         max = my_list[0]
         for i, val in enumerate(my_list):
             if val > max:
                 max = val
-    return max
+        return max
+
+    else:
+        return None
