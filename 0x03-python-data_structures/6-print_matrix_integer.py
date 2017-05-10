ee605ea36b3f873c1850473642118b6d3e4a3df4
Spencer <spencerhcheng@gmail.com>
2017-05-10 19:46:58 +0000

    add condition if matrix is empty list of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
+    if matrix is not None or matrix != [[]]:
         for x in matrix:
             for (i, val) in enumerate(x):
                 if i < len(x) - 1:
