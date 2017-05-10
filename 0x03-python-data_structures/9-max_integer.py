18946302b433ee9c7844e50ec9af7ec065bc66ba
Spencer <spencerhcheng@gmail.com>
2017-05-10 21:42:48 +0000

    simplify else condition if given empty list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if my_list == []:
-        return None
-
-    else:
+    if my_list != []:
         max = my_list[0]
         for i, val in enumerate(my_list):
             if val > max:
                 max = val
         return max
+    else:
+        return None
