766f660550bf3c60b530d8a1fbf7256034d86666
Spencer Cheng <spcheng0412@gmail.com>
2017-05-10 05:27:30 +0000

    add condition on line 6 if list is empty

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
 
     if idx <= 0 or idx >= len(my_list):
         return my_list
-    else:
+    elif my_list is not None:
         for i, ele in enumerate(my_list):
             if i == idx:
                 del my_list[idx]
