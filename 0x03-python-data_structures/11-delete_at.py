ef0ecb3ba2af42a9733dcce675df194b741a4fdd
Spencer Cheng <spcheng0412@gmail.com>
2017-05-10 05:24:31 +0000

    remove new_list, incorporate delete index, and enumerate list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = []
 
-    for i in range(len(my_list)):
-        if (i == idx):
-            continue
-        else:
-            new_list.append(my_list[i])
-    return new_list
+    if idx <= 0 or idx >= len(my_list):
+        return my_list
+    else:
+        for i, ele in enumerate(my_list):
+            if i == idx:
+                del my_list[idx]
+            else:
+                 my_list[i] = ele
+        return my_list
