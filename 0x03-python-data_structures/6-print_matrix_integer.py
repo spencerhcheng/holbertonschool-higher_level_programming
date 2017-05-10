ededa685a55299ffec2b9a06d3b564489779d277
Spencer Cheng <spcheng0412@gmail.com>
2017-05-10 06:11:25 +0000

    print in multiple rows

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        if (i, val) 
-            print("{:d} ".format(i), end="")
+
+        for x in matrix:
+            for (i, val) in enumerate(x):
+                if i < len(x) - 1:
+                    print("{:d} ".format(val), end="")
+                else:
+                    print("{:d}".format(val))
