e1e06b09a842deff6f8e28c5d79f3a3aa08c2b3c
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-10 01:49:52 +0000

    set max equal to first element in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-
     if my_list is not None:
-        max = 0
+        max = my_list[0]
         for i, val in enumerate(my_list):
             if val > max:
                 max = val
