da8f8a0cad9335ada19a868569cd6b5169b88501
Spencer <spencerhcheng@gmail.com>
2017-05-12 02:18:49 +0000

    compare sets, find diff, and remove diff from combo

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = set()
-    set_combo = set_1.union(set_2)
-    for x in set_combo:
-        for y in set_1:
-            for z in set_2:
-                if z == y and y == x:
-                    uniq = z
-                    set_combo.remove(uniq)
-                    return set_combo
+    set_combo = sorted(set_1.union(set_2))
+    common_val = set_1 - (set(set_1) - set(set_2))
+    for i in set_combo:
+        for x in common_val:
+            if x == i:
+                set_combo.remove(i)
+    return set_combo
+             
