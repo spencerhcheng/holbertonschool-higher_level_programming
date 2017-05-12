a5eb70698b78c85eea6819316cfcdbdf398c37dc
Spencer <spencerhcheng@gmail.com>
2017-05-12 01:41:33 +0000

    removed test if scores are all 0

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict == None:
+    if my_dict is None:
         return None
 
     dict_list = list(my_dict.values())
-    
-    for sum_of_num in dict_list:
-        sum_of_num += sum_of_num
-
-    if sum_of_num == 0:
-        return None
 
     max = dict_list[0]
 
     for key, val in my_dict.items():
-        if my_dict[key] > max:
-            max = my_dict[key]
+        if val > max:
+            max = val
             if val == max:
                 return key
