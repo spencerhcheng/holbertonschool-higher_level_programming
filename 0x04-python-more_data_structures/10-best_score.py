de9f78eb655d2a27df7563848858b329ecf3e0ea
Spencer <spencerhcheng@gmail.com>
2017-05-12 01:49:12 +0000

    use max built-in. simplify code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def best_score(my_dict):
+
     if my_dict is None:
         return None
 
     dict_list = list(my_dict.values())
 
-    max = dict_list[0]
-
     for key, val in my_dict.items():
-        if val > max:
-            max = val
-            if val == max:
-                return key
+        if val == max(dict_list):
+            return key
