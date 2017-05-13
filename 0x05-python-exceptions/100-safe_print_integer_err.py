7f7d927be447321d373f72e6da038a855219f166
Spencer Cheng <spencerhcheng@gmail.com>
2017-05-13 04:18:02 +0000

    add paranthesis arround exception errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
         return True
-    except ValueError, TypeError as e:
+    except (ValueError, TypeError) as e:
         stderr.write("Exception: " + str(e) + "\n")
         return False
