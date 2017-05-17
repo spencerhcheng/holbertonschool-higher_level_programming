74b8799dd0409ecc0bb52b072707ee6d9f28a0bc
Spencer <spencerhcheng@gmail.com>
2017-05-17 21:53:51 +0000

    deletions

--- 0x06-python-test_driven_development/0-add_integer3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def add_integer(a, b):
-    if isinstance(a, float)
-        if isinstance(b, float):
-            return int(a) + int(b)
-        else:
-            raise TypeError("b must be an integer")
-    else:
-        raise TypeError("a must be an integer")
