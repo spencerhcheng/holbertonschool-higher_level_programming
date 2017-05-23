fa56d58066237e39e669b2153d3897dea98c9f6a
Spencer <spencerhcheng@gmail.com>
2017-05-23 02:04:34 +0000

    add conditions on size in property setter

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
-            raise TypeError("size must be an integer")
+        self.__size = size
 
     @property
     def size(self):
-        return self.__size 
+        if isinstance(self.__size, int):
+            if self.__size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                return self.__size
+        else:
+            raise TypeError("size must be an integer")
 
     @size.setter
     def size(self, value):
@@ -19,9 +19,4 @@ class Square:
 
     def area(self):
         if isinstance(self.__size, int):
-            if self.__size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                return int(self.__size) ** 2
-        else:
-            raise TypeError("size must be an integer")
+            return int(self.__size) ** 2
