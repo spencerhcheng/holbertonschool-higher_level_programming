2a2f20be5893de88f75d1ed13ef0406f344eb594
Spencer <spencerhcheng@gmail.com>
2017-05-23 01:57:51 +0000

    remove trailing whitespace on line 10

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -7,4 +7,4 @@ class Square:
             else:
                 self.__size = size
         else:
-            raise TypeError("size must be an integer") 
+            raise TypeError("size must be an integer")
