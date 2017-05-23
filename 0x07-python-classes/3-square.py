e5ae45a89b28eb043917b899c77162b6a3e5ed43
Spencer <spencerhcheng@gmail.com>
2017-05-23 01:59:10 +0000

    add blank line before def area(self): on line 11 and remove trailing whitespace on line 13

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -8,5 +8,6 @@ class Square:
                 self.__size = size
         else:
             raise TypeError("size must be an integer")
+
     def area(self):
-        return int(self.__size) ** 2 
+        return int(self.__size) ** 2
