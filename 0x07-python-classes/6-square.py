61a31251924d2a2ac3a71042a01a83a6db7a2170
Spencer <spencerhcheng@gmail.com>
2017-05-23 03:02:55 +0000

    FINALLY GOT TO PRINT IT

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0, position=(0,0)):
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
-            raise TypeError("size must be an integer")
-        if not all(isinstance(x, int)) for x in position or x in position < 0:
-            raise TypeError("position must be a tuple of 2 positive numbers")
-        else:
-            self.__position = position
- 
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
     @property
     def size(self):
-        return self.__size 
+        return self.__size
 
     @size.setter
     def size(self, value):
-        self.__size = value
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-    @prop.position
+    @property
     def position(self):
         return self.__position
 
     @position.setter
     def position(self, value):
-        self.__position = value
+        if not (type(value[0] == int) and type(value[1] == int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
-        if isinstance(self.__size, int):
-            if self.__size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                return int(self.__size) ** 2
-        else:
-            raise TypeError("size must be an integer")
+        return int(self.__size) ** 2
 
     def my_print(self):
         if self.__size == 0:
             print('')
+
+        for y in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
-            for x in range(self.__size):
+            for x in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
                 print("#", end="")
             print("")
