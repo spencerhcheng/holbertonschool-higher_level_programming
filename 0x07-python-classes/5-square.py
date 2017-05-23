b3b3c925d788e8904a15d289eb85cf371fae0acb
Spencer <spencerhcheng@gmail.com>
2017-05-23 02:14:11 +0000

    remove whitespace oon line 5, move size conditions to ls

--- 0x07-python-classes/5-square.py
+++ 0x07-python-classes/5-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
-            raise TypeError("size must be an integer")
-    
+        self.__size = size
+
     @property
     def size(self):
-        return self.__size 
+        if isinstance(self.__size, int):
+            if self.__size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                return self.__size
+        else:
+            raise TypeError("size must be an integer")
 
     @size.setter
     def size(self, value):
@@ -19,12 +19,7 @@ class Square:
 
     def area(self):
         if isinstance(self.__size, int):
-            if self.__size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                return int(self.__size) ** 2
-        else:
-            raise TypeError("size must be an integer")
+            return int(self.__size) ** 2
 
     def my_print(self):
         if self.__size == 0:
@@ -33,5 +28,3 @@ class Square:
             for x in range(self.__size):
                 print("#", end="")
             print("")
-
-
