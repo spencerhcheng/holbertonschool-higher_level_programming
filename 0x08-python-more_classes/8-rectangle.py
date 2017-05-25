8b845a4d8e1da91f1f7688621afc2739c98bfe11
Spencer <spencerhcheng@gmail.com>
2017-05-25 05:02:23 +0000

    Add TypeError is rect_1 or rect_2 is not of class Rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -60,9 +60,13 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if (rect_1.area() > rect_2.area()):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() > rect_2.area():
             return rect_1
-        elif (rect_2.area() > rect_1.area()):
+        elif rect_2.area() > rect_1.area():
             return rect_2
         else:
             return rect_1
