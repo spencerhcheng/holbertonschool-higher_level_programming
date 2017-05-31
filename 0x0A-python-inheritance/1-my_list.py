578e6ccc5a1ed946e68ccc02448760fb82387407
Spencer <spencerhcheng@gmail.com>
2017-05-31 05:04:35 +0000

    1-my_list: initial commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+class MyList(list):
+    new_list = []
+
+    def print_sorted(self):
+        self.new_list.append(self)
+        for x in self.new_list:
+            print(sorted(x))
