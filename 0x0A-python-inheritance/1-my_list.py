0bace383c62dc6d32a81eda35ab31a8f716a5ffe
Spencer <spencerhcheng@gmail.com>
2017-06-02 20:32:14 +0000

    Condensed and returned sorted (self)

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class MyList(list):
-    new_list = []
-
     def print_sorted(self):
-        self.new_list.append(self)
-        for x in self.new_list:
-            print(sorted(x))
+        print(sorted(self))
