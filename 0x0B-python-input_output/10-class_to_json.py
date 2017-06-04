615927b2e349106d8ea4136b0925c67fd8a28296
Spencer <spencerhcheng@gmail.com>
2017-06-04 04:58:29 +0000

    Remove import to comply with specifications and return obj.__dict__

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 
 
 def class_to_json(obj):
@@ -7,5 +6,4 @@ def class_to_json(obj):
     Returns the dictionary description with simple
     data structure for JSON serialization of an object
     """
-    serialized = json.dumps(obj.__dict__)
-    return(json.loads(serialized))
+    return(obj.__dict__)
