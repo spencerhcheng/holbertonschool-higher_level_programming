c4eb693621657f11e86660aa9e95257eeb281a65
Spencer <spencerhcheng@gmail.com>
2017-06-04 05:45:58 +0000

    Return dict for attributes found in class

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
+        """
+        Create public instance attributes
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs != None:
+        """
+        Retrieve only attributes contained in class
+        """
+        partial_dict = {}
+        if attrs is not None:
             for x in range(len(attrs)):
                 try:
                     f = getattr(self, attrs[x])
-                    return json.dumps(f)
+                    partial_dict[attrs[x]] = f
                 except:
                     pass
+            return partial_dict
         else:
-            f = json.dumps(self.__dict__)
-            return json.loads(f)
-
+            return self.__dict__
