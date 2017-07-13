247dc63386ecfd75a4e3714674c5702713219a10
Spencer Cheng <spcheng0412@gmail.com>
2017-07-13 02:29:23 +0000

    10-top_score: initial commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records from table second_table from database hbtn_0c_0
+-- Displays only score and name ordered by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
