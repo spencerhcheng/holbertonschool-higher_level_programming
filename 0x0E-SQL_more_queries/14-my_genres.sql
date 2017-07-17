c748e24cde31df5a0f1cb71a4940530fca910018
Spencer <spencerhcheng@gmail.com>
2017-07-17 06:49:34 +0000

    14-my_genres: first commit

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,9 @@
 -- Script that useres the hbtn_0d_tvshows database to list all genres of the show Dexter
-
+SELECT tv_genres.name as name
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+LEFT JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name
