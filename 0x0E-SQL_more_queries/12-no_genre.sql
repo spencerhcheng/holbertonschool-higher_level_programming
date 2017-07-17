1d80ec17ce8eba8a1416cadc6a290f9d1ff90866
Spencer Cheng <spcheng0412@gmail.com>
2017-07-17 02:43:11 +0000

    12-no_genre: first commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,7 @@
 -- Import of the database dump from hbtn_0d_tvshows to MySQL server
 -- Lists all shows contained in the hbtn_0d_tvshows without a genre linked
-
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
