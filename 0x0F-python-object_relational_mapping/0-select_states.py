eda1422c1f462649f57f89ffa99a1044019adedf
Spencer <spencerhcheng@gmail.com>
2017-08-10 22:24:10 +0000

    0-select_states: code should not execute when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host="localhost",
-                     port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-c.execute("SELECT states.id, states.name FROM states ORDER BY states.id ASC")
-rows = c.fetchall()
-for x in rows:
-    print(x)
-c.close()
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT states.id, states.name\
+              FROM states ORDER BY states.id ASC")
+    rows = c.fetchall()
+    for x in rows:
+        print(x)
+    c.close()
+    db.close()
