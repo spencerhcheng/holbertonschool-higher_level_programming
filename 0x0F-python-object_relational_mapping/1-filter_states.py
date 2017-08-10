f89051025a6edca9ec3282185ff91d57dd341946
Spencer <spencerhcheng@gmail.com>
2017-08-10 22:47:25 +0000

    1-filter_states: initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Lists states that start with N from the database hbtn_0e_0_usa
+'''
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT states.id, states.name\
+              FROM states WHERE states.name like 'N%' ORDER BY states.id ASC")
+    rows = c.fetchall()
+    for x in rows:
+        print(x)
+    c.close()
+    db.close()
