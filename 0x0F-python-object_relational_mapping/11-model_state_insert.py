21a1b945f60f60ed90c1097d1c4615c61d1edc0f
Spencer <spencerhcheng@gmail.com>
2017-08-12 07:11:36 +0000

    11-model_state_insert: remove spaces arount =

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -16,8 +17,8 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    newState = State(name = 'Louisiana')
+    newState = State(name='Louisiana')
     session.add(newState)
-    session.flush()
+    session.commit()
     print("{}".format(newState.id))
     session.close()
