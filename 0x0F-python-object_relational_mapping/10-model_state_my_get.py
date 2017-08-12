949aae9f601ecf079f5dd969e77d7f15f618a445
Spencer <spencerhcheng@gmail.com>
2017-08-12 06:23:55 +0000

    10-model_state_my_get: initial commit

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the name
+ passed as arguement from the database hbtn_0e_6_usa
+"""
+if __name__ == '__main__':
+    import sqlalchemy
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.like(sys.argv[4])):
+        try:
+            print("{}".format(state.id))
+        except:
+            print("Not found")
+    session.close()
