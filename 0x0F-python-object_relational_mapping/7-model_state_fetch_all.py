3743bf95cc2e3e197498d0077d5b55295e988332
Spencer <spencerhcheng@gmail.com>
2017-08-12 01:46:24 +0000

    7-model_state_fetch_all: fix line too long PEP8 issue

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
