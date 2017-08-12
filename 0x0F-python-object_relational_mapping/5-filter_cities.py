51b41430bd0df96d3147b8cbfc5c8b918bee9178
Spencer <spencerhcheng@gmail.com>
2017-08-12 00:15:37 +0000

    5-filter_cities.py: remove spaces before after = operator

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-'''
-Lists all cities by state from the database hbtn_0e_4_usa
-'''
-import MySQLdb
-import sys
+"""
+Lists all cities by supplied state from the database hbtn_0e_4_usa
+"""
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
     db = MySQLdb.connect(host="localhost",
                          port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT cities.name\
               FROM cities JOIN states ON states.id=cities.state_id\
-              WHERE BINARY states.name = '{}'\
+              WHERE BINARY states.name='{}'\
               ORDER BY cities.id ASC".format(sys.argv[4]))
     rows = c.fetchall()
     result = []
-    for x in rows:
-        result.append(x[0])
+    for city_name in rows:
+        result.append(city_name[0])
     print (', '.join(result))
     c.close()
     db.close()
