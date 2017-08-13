e648e55c960c721c16d85d5e106735ea9fa8b0c5
Spencer <spencerhcheng@gmail.com>
2017-08-13 02:33:40 +0000

    model_city: initial commit

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Contains class definition of State and an instance
+Base = declarative_base():
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           unique=True, autoincrement=True,
+                           primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    state_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('states.id'), nullable=False)
