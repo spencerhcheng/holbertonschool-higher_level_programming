77c22fcdc31445f2b581063fe783e48705815997
Spencer <spencerhcheng@gmail.com>
2017-08-22 20:17:42 +0000

    Change form flag to data flag on curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the URL with the specified key and variable names
-curl -sF "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" "$1"
+curl -sd "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
