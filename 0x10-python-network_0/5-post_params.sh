3f7373e3e972d6334c305d4604cda910ec720fc0
Spencer <spencerhcheng@gmail.com>
2017-08-22 19:42:14 +0000

    5-post_params: initial commit

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Sends a POST request to the URL with the specified key and variable names
+curl -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" "$1"
