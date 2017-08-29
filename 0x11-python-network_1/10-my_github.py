c4d918fb932b29c32b57a55c8b97d9253c97b2d3
Spencer <spencerhcheng@gmail.com>
2017-08-29 05:51:13 +0000

    10-my_github: initial commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    try:
+        r = requests.get(url, auth=(username, password))
+        print(r.json()['id'])
+    except:
+        print("None")
