cd38b1beca71cf0496c7b13e1403dc7548980e08
spencerhcheng <spencerhcheng@gmail.com>
2017-09-19 21:26:51 +0000

    Change Number() func to parseInt() func to convert arg to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let myArgs = process.argv.slice(2);
 
-if (!isNaN(Number(myArgs[0]))) {
+if (!isNaN(parseInt(myArgs[0]))) {
   console.log((myArgs[0] >> 0));
 } else {
   console.log('Not a number');
