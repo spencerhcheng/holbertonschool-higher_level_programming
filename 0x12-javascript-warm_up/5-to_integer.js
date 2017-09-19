6eb1833b4dc4c8561e8044a7b7a0df6ec9a6760b
spencerhcheng <spencerhcheng@gmail.com>
2017-09-19 21:31:08 +0000

    Add print string of 'My number: '

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 let myArgs = process.argv.slice(2);
 
 if (!isNaN(parseInt(myArgs[0]))) {
-  console.log((myArgs[0] >> 0));
+  console.log('My number: %d', (myArgs[0] >> 0));
 } else {
   console.log('Not a number');
 }
