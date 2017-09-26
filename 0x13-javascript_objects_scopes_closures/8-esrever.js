8870a81fa55f2262b54f1fadbe7d1885a081aa0f
spencerhcheng <spencerhcheng@gmail.com>
2017-09-26 03:38:21 +0000

    8-esrever: initial commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let newList = [];
+  let i = list.length - 1;
+
+  while (i >= 0) {
+    newList.push(list[i]);
+    i--;
+  }
+  console.log(newList);
+};
