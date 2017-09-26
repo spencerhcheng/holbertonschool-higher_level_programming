a6dc87e88345f34f3e8334fca2d0302afb7dedb2
spencerhcheng <spencerhcheng@gmail.com>
2017-09-26 21:36:07 +0000

    4-starwars_count: initial commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+let myUrl = process.argv[2];
+const request = require('request');
+let count = 0;
+request.get(myUrl, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let json = JSON.parse(body);
+  let filmsList = json['results'];
+  for (let i = 0; i < filmsList.length; i++) {
+    let charList = filmsList[i]['characters'];
+    for (let j = 0; j < charList.length; j++) {
+      if (charList[j] === 'https://swapi.co/api/people/18/') {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
