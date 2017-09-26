830389fc7cdd8265ea79e335e7d43b81fa347379
spencerhcheng <spencerhcheng@gmail.com>
2017-09-26 23:22:26 +0000

    Semistandard fix: camelcase

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ let epNumber = process.argv[2];
 let myUrl = 'http://swapi.co/api/films/' + epNumber;
 const request = require('request');
 
-request.get(myUrl, function (error, response, url_body) {
+request.get(myUrl, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  let json = JSON.parse(url_body);
+  let json = JSON.parse(body);
   console.log(json['title']);
 });
