d0d7c61bdd056956ac6a8296585b8a479c988b5f
spencerhcheng <spencerhcheng@gmail.com>
2017-09-26 20:39:08 +0000

    Add error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,9 @@
 let myUrl = process.argv[2];
 const request = require('request');
 
-request.get(myUrl, function (res) {
+request.get(myUrl, function (err, res) {
+  if (err) {
+    console.log(err);
+  }
   console.log('code: %s', res.statusCode);
 });
