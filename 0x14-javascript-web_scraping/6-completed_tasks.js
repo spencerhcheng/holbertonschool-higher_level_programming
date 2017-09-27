7115332524208b173127f8fe104bb0f440faecdd
spencerhcheng <spencerhcheng@gmail.com>
2017-09-27 03:36:49 +0000

    6-completed_tasks: initial commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+let myUrl = 'https://jsonplaceholder.typicode.com/todos';
+let idObj = {};
+const request = require('request');
+
+request.get(myUrl, function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  let json = JSON.parse(body);
+  for (let i = 0; i < json.length; i++) {
+    let flag = 0;
+    let idNum = json[i]['userId'];
+    let compTasks = json[i]['completed'];
+    if (!(idObj.hasOwnProperty(idNum))) {
+      if (compTasks === false) {
+        idObj[idNum] = 0;
+      } else if (compTasks === true) {
+        flag = 1;
+        idObj[idNum] = 1;
+      }
+    }
+    if (idObj.hasOwnProperty(idNum) && compTasks === true && flag !== 1) {
+      idObj[idNum] += 1;
+    }
+  }
+  console.log(idObj);
+});
